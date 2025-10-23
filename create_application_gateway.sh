2264dc646ffc84150fcafd8f983de0e621b7a62a
Angel Javier Valdez <angel_java@hotmail.com>
2025-10-23 12:47:35 -0600

    Ajustes

--- create_application_gateway.sh
+++ create_application_gateway.sh
@@ -1,37 +1,48 @@
 #!/bin/bash
-
+# Variables de configuración
+location="mexicocentral"
+region="mx"
+project="cf"
+env="dev"
+rgName="rg-$project-$env-$region"
+pipAg="pip-ag-$project-$env-$region"
+agName="ag-$project-$env-$region"
+vnetName="vnet-$project-$env-$region"
+frontSubnetName="snet-front-$project-$env-$region"
+backSubnetName="snet-back-$project-$env-$region"
+vmName="vm-$project-$env-$region-01"
 # Crear IP pública para el Application Gateway
 az network public-ip create \
-    --resource-group rg-cf-dev-mx \
-    --name pip-ag-cf-dev-mx \
-    --location mexicocentral \
+    --resource-group $rgName \
+    --name $pipAg \
+    --location $location \
     --sku Standard \
     --allocation-method Static
 
 # Crear el Application Gateway
 az network application-gateway create \
-    --name ag-cf-dev-mx \
-    --location mexicocentral \
-    --resource-group rg-cf-dev-mx \
-    --vnet-name vnet-cf-dev-mx \
-    --subnet snet-front-cf-dev-mx \
+    --name $agName \
+    --location $location \
+    --resource-group $rgName \
+    --vnet-name $vnetName \
+    --subnet $frontSubnetName \
     --capacity 2 \
     --sku Standard_v2 \
     --http-settings-cookie-based-affinity Disabled \
     --frontend-port 80 \
     --http-settings-port 80 \
     --http-settings-protocol Http \
-    --frontend-ip pip-ag-cf-dev-mx \
+    --frontend-ip $pipAg \
     --backend-pool-name backendPool \
-    --backend-pool-ip-address vm-cf-dev-mx-01 \
+    --backend-pool-ip-address $vmName \
     --routing-rule-name rule \
     --listener-name listener \
     --http-settings-name httpSetting
 
 # Mostrar IP pública del Application Gateway
 agIp=$(az network public-ip show \
-    --resource-group rg-cf-dev-mx \
-    --name pip-ag-cf-dev-mx \
+    --resource-group $rgName \
+    --name $pipAg \
     --query ipAddress \
     --output tsv)
 
